9252dde2caf98f11a16f0ef56b7a683ae48a8a63
Thibaut W <tibo7@gmail.com>
2024-04-02 13:29:30 +0200

    11000+ movies id & url scraped from IMDB

--- scraping/moviescraping/spiders/imdb_movies.py
+++ scraping/moviescraping/spiders/imdb_movies.py
@@ -14,7 +14,7 @@ class ImdbMoviesSpider(CrawlSpider):
     driver = webdriver.Chrome()
         
     def start_requests(self): # URL -> french audio movies (no documentaries, no shorts) from 2021 to 2025
-        url = "https://www.imdb.com/search/title/?title_type=feature&release_date=2021-05-19,2024-12-31&genres=!documentary,!short&languages=fr"
+        url = "https://www.imdb.com/search/title/?title_type=feature&release_date=2000-01-01,2024-12-31&genres=!documentary,!short&languages=fr"
         yield SeleniumRequest(url=url, callback=self.parse)
 
     @logger.catch
@@ -31,10 +31,11 @@ class ImdbMoviesSpider(CrawlSpider):
         try:
             while True:
                 button = self.driver.find_element(By.XPATH, '//main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[2]/div[2]/div/span/button')
+
                 # Scroll down the page until you get to the element using JavaScript
                 self.driver.execute_script("arguments[0].scrollIntoView();", button)
-                time.sleep(3) # wait 2s
                 # If there's a button "see more" -> click on it
+                time.sleep(3) # wait 2s
                 button.click()
                 time.sleep(3)
         except NoSuchElementException:
@@ -50,20 +51,20 @@ class ImdbMoviesSpider(CrawlSpider):
     @logger.catch
     def parse_movie_page(self, response):
         
-        data = response.meta.get('data')
+        # data = response.meta.get('data')
         
         # Reload driver with the movie url
-        self.driver.get(response.url)
+        # self.driver.get(response.url)
         
         # Scrap all the information from every movie page
-        movie_title = self.driver.find_element(By.XPATH, '//span[@data-testid="hero__primary-text"]')
+        # movie_title = self.driver.find_element(By.XPATH, '//span[@data-testid="hero__primary-text"]')
         # original_title = self.driver.find_element(By.XPATH, '//main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/div')
         # release_date = self.driver.find_element(By.XPATH, '//a[@class="ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link"]')
         
         yield {
             'movie_url': response.url,
             'movie_id': re.search(r'tt\d+', str(response.url)).group(),
-            'movie_title' : movie_title.text,
+            # 'movie_title' : movie_title.text,
             # 'original_title' : original_title.text,
             # 'release_date' : release_date.text
             
