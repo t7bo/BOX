584ec5baaa1841b180b48fb187f4775dcb9e38a7
Thibaut W <tibo7@gmail.com>
2024-04-04 13:27:57 +0200

    Top100 celebrities scraped from IMDB

--- scraping/moviescraping/spiders/imdb_most_popular_celebs.py
+++ scraping/moviescraping/spiders/imdb_most_popular_celebs.py
@@ -1,10 +1,19 @@
 import scrapy
 
-
 class ImdbMostPopularCelebsSpider(scrapy.Spider):
     name = "imdb_most_popular_celebs"
     allowed_domains = ["www.imdb.com"]
     start_urls = ["https://www.imdb.com/chart/starmeter/?ref_=nv_cel_m"]
 
+    # custom_settings = {
+
+    #     'ITEM_PIPELINES' : {
+    #         "moviescraping.pipelines.MoviesPipeline": 300
+    #     }
+    # }
+
     def parse(self, response):
-        pass
+
+        yield {
+            'celebrity' : response.xpath('//div[@data-testid="nlib-title"]/a/h3/text()').getall()
+        }
